f41d6a3fb0ad174a4441caa505ac853f5ef088a6
Adonis <adonisjr.suico@gmail.com>
2025-10-07 00:24:43 +0800

    chore: added new feature for courses, curriculums and subjects

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UploadedTorController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CurriculumController;
+use App\Http\Controllers\SubjectController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -17,5 +20,9 @@ Route::middleware('auth-ocr')->group(function () {
 
     Route::apiResource('/users', UserController::class);
     Route::apiResource('/tor', UploadedTorController::class);
+
+    Route::apiResource('courses', CourseController::class);
+    Route::apiResource('curriculums', CurriculumController::class);
+    Route::apiResource('subjects', SubjectController::class);
     // Route::get('/dashboard', [AuthController::class, 'dashboard']);
 });
